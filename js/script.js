e6582ee465115fa711c7fce4380e69569984cbd7
Jason Chun <jchun711@gmail.com>
2022-03-24 22:41:03 -0400

    cloned github repo into local machine and created html and javascript files. didnt make css file as ill try to do the style all on js. created the nav with a link for high scores and a timer that will show once the start button in clicked. put an h2 for the title and an h4 for the text content. not sure what the questions will be on so left blank for now. made a button for start quiz, and made divs for the actually quiz, score, and high scores. link js file, and made variables for all the html elements i have so far.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var startbtn = document.querySelector('#startquiz');
+var timer = document.querySelector('#timer');
+var body = document.querySelector('card-body');
+var quiz = document.querySelector('#quiz');
+var score = document.querySelector('#score');
+var highscores = document.querySelector('#highscores');
+var scorelink = document.querySelector('#highscorelink');
