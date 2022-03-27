340f96a56de64155721229f3016925f8b1f30f32
Jason Chun <jchun711@gmail.com>
2022-03-26 22:02:44 -0400

    created a new js file with the questions for the quiz and linked it to the html. decided to do a California quiz with questions about the state which are relatively easy. created variables for seconds left, question index, correct answers, total questions, different options, ans, and previous scores, created choice array and div arrays. created functions for creating buttons for choices, the start quiz, timer function, the build questions, and the start button so the quiz can start. next is to create a function to review the results after the quiz ends, and  function to store and show high scores using local storage.

---
+++ js/questions.js
@@ -0,0 +1,22 @@
+var questions = [
+    {
+        title: 'California is located on what coast of the United States?',
+        choices: ['East Coast', 'West Coast', 'Gulf Coast', 'Arctic Coast'],
+        answer: 'West Coast'
+    },
+    {
+        title: 'Where does California rank in population compared to all other states?',
+        choices: ['first', 'second', 'third', 'fourth'],
+        answer: 'first'
+    },
+    {
+        title: 'Where does California rank in area compared to the rest of the states?',
+        choices: ['first', 'second', 'third', 'fourth'],
+        answer: 'third'
+    },
+    {
+        title: "What city is the California's state capital?",
+        choices: ['San Francisco', 'San Diego', 'Los Angeles', 'Sacramento'],
+        answer: 'Sacramento'
+    }
+];
--- js/script.js
+++ js/script.js
@@ -1,7 +1,81 @@
-var startbtn = document.querySelector('#startquiz');
+var startBtn = document.querySelector('#startquiz');
 var timer = document.querySelector('#timer');
-var body = document.querySelector('card-body');
-var quiz = document.querySelector('#quiz');
+var mainContent = document.querySelector('#mainContent');
+var questionEl = document.querySelector('#title');
+var quizContent = document.querySelector('#quiz');
+var result = document.querySelector('#answer');
 var score = document.querySelector('#score');
-var highscores = document.querySelector('#highscores');
-var scorelink = document.querySelector('#highscorelink');
+var highScores = document.querySelector('#highscores');
+var highScoreLink = document.querySelector('#highscorelink');
+var navLink = document.querySelector('highscorelink');
+
+var secondsLeft = 75, questionIndex = 0, correct = 0;
+var totalQuestions = questions.length;
+var question, option1, option2, option3, option4, ans, previousScores;
+var choiceArray = [], divArray = [];
+
+for(var i = 0; i < 4; i++) {
+    var dv = document.createElement("div");
+    var ch = document.createElement('button');
+    ch.setAttribute('data-index', i);
+    choiceArray.push(ch);
+    divArray.push(dv);
+};
+
+// start quiz
+function startQuiz() {
+    startTimer();
+    buildQuestion();
+};
+
+// timer start when quiz start
+function startTimer() {
+    var timeInterval = setInterval(function() {
+        secondsLeft--;
+        timer.textContent = 'Time : ' +secondsLeft+ ' sec';
+
+        if(secondsLeft <= 0 || (questionIndex > totalQuestions-1)){
+            result.style.display = 'none';
+            quiz.style.display = 'none';
+            viewResult();
+            clearInterval(timeInterval);
+            timer.textContent = "";
+        }
+    }, 1000);
+};
+
+function buildQuestion(){
+    questionEl.style.display = 'none';
+    mainContent.style.display = 'none';
+    quizContent.style.display = 'none';
+
+    if(questionIndex > totalQuestions -1 ) {
+        return;
+    }
+    else {
+        ans = questions[questionIndex].answer;
+
+        questionEl.innerHTML = questions[questionIndex].title;
+        questionEl.style.display = 'block';
+
+        for(var j = 0; j < 4; j++) {
+            var index = choiceArray[j].getAttribute('data-index');
+            choiceArray[j].textContent = (+index+1) + '. ' + questions[questionIndex].choices[index];
+            divArray[j].appendChild(choiceArray[j]);
+            quizContent.appendChild(divArray[j]);
+        }
+    }
+
+    quizContent.style.display = 'block';
+};
+
+highScoreLink.addEventListener('click', function() {
+    mainContent.style.display = 'none';
+    navLink.style.display = 'none';
+
+    previousScores = JSON.parse(localStorage.getItem('previousScores'));
+
+    showHighscroes();
+});
+
+startBtn.addEventListener('click', startQuiz);
